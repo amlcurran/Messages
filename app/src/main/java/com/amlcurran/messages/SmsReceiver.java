213ba51ee6288c6b31da7ecec8fb7b8bc352577a
Alex Curran <aml.curran@gmail.com>
2014-05-17 23:10:16 +0100

    Post a notification for the latest new text

--- app/src/main/java/com/amlcurran/messages/SmsReceiver.java
+++ app/src/main/java/com/amlcurran/messages/SmsReceiver.java
@@ -1,13 +1,13 @@
 package com.amlcurran.messages;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.provider.Telephony;
 import android.telephony.SmsMessage;
-import android.widget.Toast;
 
 public class SmsReceiver extends BroadcastReceiver {
 
@@ -27,7 +27,22 @@ public class SmsReceiver extends BroadcastReceiver {
         contentValues.put(Telephony.Sms.Inbox.DATE_SENT, message.getTimestampMillis());
         contentValues.put(Telephony.Sms.Inbox.TYPE, Telephony.Sms.Inbox.MESSAGE_TYPE_INBOX);
 
-        Uri inserted = context.getContentResolver().insert(Telephony.Sms.Inbox.CONTENT_URI, contentValues);
-        Toast.makeText(context, String.valueOf(inserted), Toast.LENGTH_SHORT).show();
+        context.getContentResolver().insert(Telephony.Sms.Inbox.CONTENT_URI, contentValues);
+        buildNotification(context, message.getDisplayOriginatingAddress(), message.getDisplayMessageBody(), message.getTimestampMillis());
+    }
+
+    private void buildNotification(Context context, String displayOriginatingAddress, String displayMessageBody, long timestampMillis) {
+        Notification.BigTextStyle style = new Notification.BigTextStyle();
+        style.bigText(displayMessageBody)
+                .setBigContentTitle(displayOriginatingAddress)
+                .setSummaryText(displayMessageBody);
+        Notification notification = new Notification.Builder(context)
+                .setContentTitle(displayOriginatingAddress)
+                .setSmallIcon(R.drawable.ic_notify_sms)
+                .setStyle(style)
+                .setWhen(timestampMillis)
+                .build();
+        ((NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE))
+                .notify(1, notification);
     }
 }
