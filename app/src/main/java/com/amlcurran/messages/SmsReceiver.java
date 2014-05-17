2526c7c6be69a882bedaef5cc19c4a79fe8532fc
Alex Curran <aml.curran@gmail.com>
2014-05-17 19:02:27 +0100

    Receive SMS properly

--- app/src/main/java/com/amlcurran/messages/SmsReceiver.java
+++ app/src/main/java/com/amlcurran/messages/SmsReceiver.java
@@ -1,12 +1,32 @@
 package com.amlcurran.messages;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
 
 public class SmsReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+        for (SmsMessage message : messages) {
+            writeSmsToProvider(context, message);
+        }
+    }
 
+    private void writeSmsToProvider(Context context, SmsMessage message) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Telephony.Sms.Inbox.BODY, message.getDisplayMessageBody());
+        contentValues.put(Telephony.Sms.Inbox.ADDRESS, message.getDisplayOriginatingAddress());
+        contentValues.put(Telephony.Sms.Inbox.DATE, message.getTimestampMillis());
+        contentValues.put(Telephony.Sms.Inbox.DATE_SENT, message.getTimestampMillis());
+        contentValues.put(Telephony.Sms.Inbox.TYPE, Telephony.Sms.Inbox.MESSAGE_TYPE_INBOX);
+
+        Uri inserted = context.getContentResolver().insert(Telephony.Sms.Inbox.CONTENT_URI, contentValues);
+        Toast.makeText(context, String.valueOf(inserted), Toast.LENGTH_SHORT).show();
     }
 }
