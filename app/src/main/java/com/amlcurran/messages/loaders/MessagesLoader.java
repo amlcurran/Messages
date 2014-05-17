7f989796106ecd2e9a4fa923d0d2e8a091676c5c
Alex Curran <aml.curran@gmail.com>
2014-05-17 11:36:31 +0100

    Add (incorrect) person lookup logic

--- app/src/main/java/com/amlcurran/messages/loaders/MessagesLoader.java
+++ app/src/main/java/com/amlcurran/messages/loaders/MessagesLoader.java
@@ -3,8 +3,15 @@ package com.amlcurran.messages.loaders;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.provider.Telephony;
+import android.text.TextUtils;
+import android.util.Log;
 
+import com.amlcurran.messages.adapters.CursorHelper;
+
+import java.util.Arrays;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 
 public class MessagesLoader {
@@ -22,23 +29,86 @@ public class MessagesLoader {
     }
 
     public void loadConversationList(final CursorLoadListener loadListener) {
-        executor.submit(new Runnable() {
+        executor.submit(new Callable<Object>() {
+
             @Override
-            public void run() {
-                final Cursor cursor = getResolver().query(Telephony.Threads.CONTENT_URI, null, null, null, Telephony.Sms.DEFAULT_SORT_ORDER);
-                activity.runOnUiThread(notifyListener(loadListener, cursor));
+            public Object call() throws Exception {
+                Cursor conversationsList = getResolver().query(Telephony.Threads.CONTENT_URI, null, null, null, Telephony.Sms.DEFAULT_SORT_ORDER);
+//                Cursor peopleList = queryPeople(conversationsList);
+//                //peopleList.getExtras().putInt("key", 1);
+//
+//                while (peopleList.moveToNext()) {
+//                    Log.d("Testing", CursorHelper.fromColumn(peopleList, ContactsContract.Contacts.DISPLAY_NAME_PRIMARY));
+//                }
+
+                activity.runOnUiThread(notifyListener(loadListener, conversationsList));
+                //activity.runOnUiThread(notifyListener(loadListener, peopleList));
+                return null;
             }
         });
     }
 
+    private Cursor queryPeople(Cursor conversationsList) {
+        String personSelection = createPersonSelectionString(conversationsList);
+        String[] personSelectArgs = createPersonSelectArgs(conversationsList);
+        resetCursorPosition(conversationsList);
+        String[] projection = new String[] {ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME_PRIMARY };
+
+        return getResolver().query(ContactsContract.Data.CONTENT_URI, projection, personSelection, personSelectArgs, null);
+    }
+
+    private boolean resetCursorPosition(Cursor conversationsList) {
+        return conversationsList.moveToPosition(-1);
+    }
+
+    private String[] createPersonSelectArgs(Cursor conversationsList) {
+        String[] firstPass = new String[conversationsList.getCount()];
+        int currentPosition = 0;
+        resetCursorPosition(conversationsList);
+        while (conversationsList.moveToNext()) {
+            if (isValidPersonId(conversationsList)) {
+                firstPass[currentPosition] = CursorHelper.fromColumn(conversationsList, Telephony.Sms.PERSON);
+                currentPosition++;
+            }
+        }
+
+        String[] result = Arrays.copyOf(firstPass, currentPosition);
+
+        Log.d("Selection string", "" + currentPosition + " result length " + result.length );
+        return result;
+    }
+
+    private String createPersonSelectionString(Cursor conversationsList) {
+        String personSelection = "";
+        int count = 0;
+        resetCursorPosition(conversationsList);
+        while (conversationsList.moveToNext()) {
+            if (isValidPersonId(conversationsList)) {
+                personSelection += ContactsContract.Contacts.LOOKUP_KEY + "=? OR ";
+                count++;
+            }
+        }
+        Log.d("Selection string", "" + count);
+        if (personSelection.length() > 0) {
+            personSelection = personSelection.substring(0, personSelection.length() - 4);
+        }
+        return personSelection;
+    }
+
+    private boolean isValidPersonId(Cursor conversationsList) {
+        return CursorHelper.asInt(conversationsList, Telephony.Sms.PERSON) != -1  && !TextUtils.isEmpty(CursorHelper.fromColumn(conversationsList, Telephony.Sms.PERSON));
+    }
+
     public void loadThread(final String threadId, final CursorLoadListener loadListener) {
-        executor.submit(new Runnable() {
+        executor.submit(new Callable<Object>() {
+
             @Override
-            public void run() {
+            public Object call() throws Exception {
                 String selection = Telephony.Sms.THREAD_ID + "=?";
                 String[] selectionArgs = {threadId};
                 final Cursor cursor = getResolver().query(Telephony.Sms.CONTENT_URI, null, selection, selectionArgs, Telephony.Sms.DEFAULT_SORT_ORDER.replace("DESC", "ASC"));
                 activity.runOnUiThread(notifyListener(loadListener, cursor));
+                return null;
             }
         });
     }
