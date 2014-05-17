0e31e649551da9011577f61fee70d9103ff7db9c
Alex Curran <aml.curran@gmail.com>
2014-05-17 17:11:10 +0100

    Thread fragment has send button and compose field

--- app/src/main/java/com/amlcurran/messages/ThreadFragment.java
+++ app/src/main/java/com/amlcurran/messages/ThreadFragment.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.amlcurran.messages.adapters.AdaptiveCursorSource;
@@ -28,6 +29,11 @@ public class ThreadFragment extends ListeningCursorListFragment<ThreadMessage> {
         fragment.setArguments(bundle);
 
         return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_thread, container, false);
     }
 
     @Override
