e23a43e8eaf6a9393bc0815ca0090cf795d91f5d
Alex Curran <aml.curran@gmail.com>
2014-05-19 20:31:27 +0100

    Make database writing synchronous again to prevent issues

--- app/src/main/java/com/amlcurran/messages/SmsDatabaseWriter.java
+++ app/src/main/java/com/amlcurran/messages/SmsDatabaseWriter.java
@@ -5,41 +5,13 @@ import android.content.ContentValues;
 import android.net.Uri;
 import android.provider.Telephony;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class SmsDatabaseWriter {
-
-    private final ExecutorService executorService;
-
-    public SmsDatabaseWriter() {
-        executorService = Executors.newSingleThreadExecutor();
-    }
-
-    private static ContentValues valuesFromMessage(SmsMessage message) {
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(Telephony.Sms.Inbox.BODY, message.getBody());
-        contentValues.put(Telephony.Sms.Inbox.ADDRESS, message.getAddress());
-        contentValues.put(Telephony.Sms.Inbox.DATE, message.getTimestamp());
-        contentValues.put(Telephony.Sms.Inbox.DATE_SENT, message.getTimestamp());
-        contentValues.put(Telephony.Sms.Inbox.TYPE, Telephony.Sms.Inbox.MESSAGE_TYPE_INBOX);
-        return contentValues;
-    }
 
     public void writeSentMessage(final ContentResolver contentResolver,
                                  final SentWriteListener sentWriteListener, SmsMessage message) {
-
         final ContentValues values = message.toContentValues(Telephony.Sms.MESSAGE_TYPE_SENT);
         values.put(Telephony.Sms.Sent.READ, "1");
-
-        executorService.submit(new Callable<Object>() {
-            @Override
-            public Object call() throws Exception {
-                writeSentSmsInternal(contentResolver, sentWriteListener, values);
-                return null;
-            }
-        });
+        writeSentSmsInternal(contentResolver, sentWriteListener, values);
     }
 
     private static void writeSentSmsInternal(ContentResolver contentResolver, SentWriteListener sentWriteListener, ContentValues values) {
@@ -53,14 +25,7 @@ public class SmsDatabaseWriter {
 
     public void writeInboxSms(final ContentResolver resolver, final InboxWriteListener inboxWriteListener, SmsMessage message) {
         final ContentValues contentValues = message.toContentValues(Telephony.Sms.Sent.MESSAGE_TYPE_INBOX);
-
-        executorService.submit(new Callable<Object>() {
-            @Override
-            public Object call() throws Exception {
-                writeInboxSmsInternal(resolver, inboxWriteListener, contentValues);
-                return null;
-            }
-        });
+        writeInboxSmsInternal(resolver, inboxWriteListener, contentValues);
     }
 
     private static void writeInboxSmsInternal(ContentResolver resolver, InboxWriteListener inboxWriteListener, ContentValues contentValues) {
