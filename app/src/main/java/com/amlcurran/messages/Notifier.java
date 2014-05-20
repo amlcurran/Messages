f92a7d9d7d284dc2927c65b02b48a4df9046ee87
Alex Curran <aml.curran@gmail.com>
2014-05-20 21:02:01 +0100

    Improved the notification

--- app/src/main/java/com/amlcurran/messages/Notifier.java
+++ app/src/main/java/com/amlcurran/messages/Notifier.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Alex Curran
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.amlcurran.messages;
 
 import android.app.Notification;
@@ -5,10 +21,15 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
 
 public class Notifier {
 
     private static final int ID_NEW_MESSAGE = 22;
+    private static final long[] VIBRATE_PATTERN = new long[] { 200 };
     private final NotificationManager notificationManager;
     private final Context context;
 
@@ -27,16 +48,24 @@ public class Notifier {
         notificationManager.cancel(ID_NEW_MESSAGE);
     }
 
-    private Notification buildNotification(Context context, String displayOriginatingAddress, String displayMessageBody, long timestampMillis) {
+    private Notification buildNotification(Context context, String address, String body, long timestampMillis) {
         Notification.BigTextStyle style = new Notification.BigTextStyle()
-                .bigText(displayMessageBody)
-                .setBigContentTitle(displayOriginatingAddress);
+                .bigText(body)
+                .setBigContentTitle(address);
         return getDefaultBuilder(context)
-                .setContentTitle(displayOriginatingAddress)
-                .setContentText(displayMessageBody)
+                .setTicker(buildTicker(address, body))
+                .setContentTitle(address)
+                .setContentText(body)
                 .setStyle(style)
                 .setWhen(timestampMillis)
                 .build();
+    }
+
+    private CharSequence buildTicker(String address, String body) {
+        SpannableStringBuilder builder = new SpannableStringBuilder(address);
+        builder.setSpan(new StyleSpan(Typeface.BOLD), 0, builder.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        builder.append(": ").append(body);
+        return builder;
     }
 
     private Notification.Builder getDefaultBuilder(Context context) {
@@ -45,6 +74,7 @@ public class Notifier {
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         return new Notification.Builder(context)
                 .setContentIntent(pendingIntent)
+                .setVibrate(VIBRATE_PATTERN)
                 .setSmallIcon(R.drawable.ic_notify_sms);
     }
 }
