83077ef4bda3f2ab95ba2a287ac0d3318ea0d797
Alex Curran <aml.curran@gmail.com>
2014-05-22 18:45:33 +0100

    Hide the list when it is loading

--- app/src/main/java/com/amlcurran/messages/conversationlist/ConversationListFragment.java
+++ app/src/main/java/com/amlcurran/messages/conversationlist/ConversationListFragment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Alex Curran
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.amlcurran.messages.conversationlist;
 
 import android.app.Activity;
@@ -72,10 +88,12 @@ public class ConversationListFragment extends ListeningCursorListFragment<Conver
 
     private void showLoadingUi() {
         ((AnimationDrawable) emptyView.getDrawable()).start();
+        getListView().setVisibility(View.GONE);
         emptyView.setVisibility(View.VISIBLE);
     }
 
     private void hideLoadingUi() {
+        getListView().setVisibility(View.VISIBLE);
         emptyView.setVisibility(View.GONE);
     }
 
