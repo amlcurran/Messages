2efc3d38776d845ff0c316fdbd086bc753e1e56a
Alex Curran <aml.curran@gmail.com>
2014-07-12 16:01:39 +0100

    fix a crash when rotating device

--- app/src/com/amlcurran/messages/ComposeNewFragment.java
+++ app/src/com/amlcurran/messages/ComposeNewFragment.java
@@ -96,6 +96,7 @@ public class ComposeNewFragment extends Fragment implements ComposeMessageView.O
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         defaultAppChecker = new DefaultAppChecker(getActivity(), composeView);
+        loader = new ProviderHelper<MessagesLoaderProvider>(MessagesLoaderProvider.class).get(getActivity()).getMessagesLoader();
         contactSource = new ArrayListSource<Contact>();
 
         adapter = new SourceBinderAdapter<Contact>(getActivity(), contactSource, new ContactBinder());
@@ -152,7 +153,6 @@ public class ComposeNewFragment extends Fragment implements ComposeMessageView.O
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         listener = new ProviderHelper<SmsComposeListener>(SmsComposeListener.class).get(activity);
-        loader = new ProviderHelper<MessagesLoaderProvider>(MessagesLoaderProvider.class).get(activity).getMessagesLoader();
     }
 
     @Override
