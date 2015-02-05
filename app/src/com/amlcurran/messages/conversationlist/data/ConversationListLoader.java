7ac30b5390c5d48beae018c578c269766de60c92
Alex Curran <aml.curran@gmail.com>
2015-02-05 22:02:54 +0000

    For now, don't add threads with a null ID to the conversation list

--- app/src/com/amlcurran/messages/conversationlist/data/ConversationListLoader.java
+++ app/src/com/amlcurran/messages/conversationlist/data/ConversationListLoader.java
@@ -23,6 +23,7 @@ import android.provider.ContactsContract;
 import android.provider.Telephony;
 import android.telephony.PhoneNumberUtils;
 
+import com.amlcurran.messages.core.Log;
 import com.amlcurran.messages.core.TextUtils;
 import com.amlcurran.messages.core.conversationlist.Conversation;
 import com.amlcurran.messages.core.data.Contact;
@@ -70,7 +71,12 @@ class ConversationListLoader {
             long lastMessageTime = CursorHelper.asLong(conversationsList, helper.getDateSentKey());
             int conversationCount = -1;
             Conversation conversation = new Conversation(contact.getNumber(), body, threadId, isRead, contact, lastFromMe, Time.fromMillis(lastMessageTime), conversationCount);
-            conversations.add(conversation);
+
+            if (threadId != null) {
+                conversations.add(conversation);
+            } else {
+                Log.getLogger().w(this, "Couldn't add a thread with null ID!!");
+            }
         }
 
         conversationsList.close();
