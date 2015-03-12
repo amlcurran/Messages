e1c9009c065d2d67e12a64ad31b1dbd7342c4197
Alex Curran <aml.curran@gmail.com>
2015-03-12 21:43:23 +0000

    Remove styling from unread conversation summary

--- app/src/com/amlcurran/messages/conversationlist/adapter/TextFormatter.java
+++ app/src/com/amlcurran/messages/conversationlist/adapter/TextFormatter.java
@@ -18,6 +18,7 @@ package com.amlcurran.messages.conversationlist.adapter;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
 import android.text.style.TextAppearanceSpan;
 
@@ -44,7 +45,9 @@ public class TextFormatter {
 
     CharSequence unread(CharSequence charSequence) {
         return new Truss().pushSpan(new StyleSpan(Typeface.BOLD))
+                .pushSpan(new ForegroundColorSpan(activity.getResources().getColor(R.color.material_text_standard_dark)))
                 .append(charSequence)
+                .popSpan()
                 .popSpan()
                 .build();
     }
