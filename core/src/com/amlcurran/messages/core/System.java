b7c1d747691a2f26bfbb9a70045af31490a999d1
Alex Curran <aml.curran@gmail.com>
2015-03-16 20:44:33 +0000

    Move new system to source tree

---
+++ core/src/com/amlcurran/messages/core/System.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2014 Alex Curran
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.amlcurran.messages.core;
+
+import com.amlcurran.messages.core.data.PhoneNumber;
+import com.amlcurran.messages.core.data.SmsMessage;
+import com.amlcurran.messages.core.threads.InFlightSmsMessage;
+import com.amlcurran.messages.core.threads.MessagePersister;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
+
+class System {
+
+    private final MessagePersister messagePersister;
+    private final Map<String, WeakReference<ThreadListener>> listenerMap;
+
+    public System(MessagePersister messagePersister) {
+        this.messagePersister = messagePersister;
+        this.listenerMap = new HashMap<>();
+    }
+
+    public void receivedMessage(InFlightSmsMessage message) {
+        messagePersister.writeIncomingMessage(message, persisterCallbacks);
+    }
+
+    public void listenTo(PhoneNumber phoneNumber, ThreadListener threadListener) {
+        listenerMap.put(phoneNumber.flatten(), new WeakReference<>(threadListener));
+    }
+
+    private MessagePersister.Callbacks persisterCallbacks = new MessagePersister.Callbacks() {
+        @Override
+        public void newMessage(SmsMessage message) {
+            if (availableListener(message.getAddress()) != null) {
+                availableListener(message.getAddress()).newMessage(message);
+            }
+        }
+
+        @Override
+        public void writeFailed() {
+
+        }
+    };
+
+    private ThreadListener availableListener(String address) {
+        return listenerMap.get(address) != null ? listenerMap.get(address).get() : null;
+    }
+
+}
--- core/tests/com/amlcurran/messages/core/IncomingSmsTests.java
+++ core/tests/com/amlcurran/messages/core/IncomingSmsTests.java
@@ -16,7 +16,6 @@
 
 package com.amlcurran.messages.core;
 
-import com.amlcurran.messages.core.data.PhoneNumber;
 import com.amlcurran.messages.core.data.SmsMessage;
 import com.amlcurran.messages.core.data.Time;
 import com.amlcurran.messages.core.threads.InFlightSmsMessage;
@@ -24,10 +23,6 @@ import com.amlcurran.messages.core.threads.MessagePersister;
 import com.amlcurran.messages.core.threads.ResultCallback;
 
 import org.junit.Test;
-
-import java.lang.ref.WeakReference;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertNull;
@@ -82,44 +77,6 @@ public class IncomingSmsTests {
         assertNull(capturingThreadListener.lastMessage);
     }
 
-    private static class System {
-
-        private final MessagePersister messagePersister;
-        private final Map<String, WeakReference<CapturingThreadListener>> listenerMap;
-
-        public System(MessagePersister messagePersister) {
-            this.messagePersister = messagePersister;
-            this.listenerMap = new HashMap<>();
-        }
-
-        public void receivedMessage(InFlightSmsMessage message) {
-            messagePersister.writeIncomingMessage(message, persisterCallbacks);
-        }
-
-        public void listenTo(PhoneNumber phoneNumber, CapturingThreadListener capturingThreadListener) {
-            listenerMap.put(phoneNumber.flatten(), new WeakReference<>(capturingThreadListener));
-        }
-
-        private MessagePersister.Callbacks persisterCallbacks = new MessagePersister.Callbacks() {
-            @Override
-            public void newMessage(SmsMessage message) {
-                if (availableListener(message.getAddress()) != null) {
-                    availableListener(message.getAddress()).newMessage(message);
-                }
-            }
-
-            @Override
-            public void writeFailed() {
-
-            }
-        };
-
-        private CapturingThreadListener availableListener(String address) {
-            return listenerMap.get(address) != null ? listenerMap.get(address).get() : null;
-        }
-
-    }
-
     private static InFlightSmsMessage incomingMessage() {
         return new InFlightSmsMessage("Some sms text", new BasicPhoneNumber("0800 289492"),
                 SmsMessage.Type.INBOX, Time.now());
@@ -140,9 +97,10 @@ public class IncomingSmsTests {
 
     }
 
-    private class CapturingThreadListener {
+    private class CapturingThreadListener implements ThreadListener {
         public SmsMessage lastMessage;
 
+        @Override
         public void newMessage(SmsMessage message) {
             lastMessage = message;
         }
